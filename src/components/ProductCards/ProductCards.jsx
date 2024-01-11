d81848f98a35dad2123671640afcb3953b23399f
UkandieSunday <ukandieudo202@gmail.com>
2024-01-11 07:38:01 +0100

    added pagination

--- src/components/ProductCards/ProductCards.jsx
+++ src/components/ProductCards/ProductCards.jsx
@@ -1,13 +1,22 @@
 import { Link } from "react-router-dom";
 import "./ProductCards.css";
-import React from "react";
+import React, { useState } from "react";
 
 const ProductCards = ({ products }) => {
-  const filteredProducts = products;
+  const [currentPage, setCurrentPage] = useState(1);
+  const pageSize = 5;
+  const startIndex = (currentPage - 1) * pageSize;
+  const endIndex = currentPage * pageSize;
+  const paginatedProducts = products.slice(startIndex, endIndex);
+  const handlePageChange = (page) => {
+    setCurrentPage(page);
+    window.scrollTo(650, 650);
+  };
+
   return (
     <div>
       <div className="cards">
-        {filteredProducts.map((product) => (
+        {paginatedProducts.map((product) => (
           <Link
             key={product.id}
             className="links card-link"
@@ -25,6 +34,29 @@ const ProductCards = ({ products }) => {
             </div>
           </Link>
         ))}
+      </div>
+      <div className="btn-container">
+        <button
+          className="add-to-cart"
+          id="btn"
+          onClick={() => {
+            handlePageChange(currentPage - 1);
+          }}
+          disabled={currentPage === 1}
+        >
+          previous
+        </button>
+        <span>{currentPage}</span>
+        <button
+          className="add-to-cart"
+          id="btn"
+          onClick={() => {
+            handlePageChange(currentPage + 1);
+          }}
+          disabled={currentPage === 3}
+        >
+          Next
+        </button>
       </div>
     </div>
   );
